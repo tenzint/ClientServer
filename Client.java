fc792adaffe562d9f70854960b12ae9a3cce3480
Tenzin Thabkhae <tenzin392@gmail.com>
2016-10-03 23:13:24 -0400

    Code pushed up; server.java cannot compile yet though

--- Client.java
+++ Client.java
@@ -1,10 +1,32 @@
-import java.io.*
-import java.net.*
+import java.io.*;
+import java.net.*;
 
 class Client
 {
 	public static void main(String args[])
 	{
-	
+		String hostName = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+		String fromServer="";
+		try {
+			Socket sock = new Socket(hostName, portNumber);
+			
+			BufferedReader kRead = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			
+			while(true)
+			{
+				String c_cmd = kRead.readLine();
+				out.println(c_cmd);
+				
+				String s_cmd = in.readLine();
+				System.out.println("Server :: " +s_cmd);
+			}
+		} catch (Exception e)
+		{
+			System.out.println("Connection lost...");
+		}
+		
 	}
 }
--- Server.java
+++ Server.java
@@ -1,11 +1,51 @@
-import java.io.*
-import java.net.*
+import java.io.*;
+import java.net.*;
 
 class Server
 {
+	Socket client;
+	Server(Socket client)
+	{
+		this.client=client;
+		Communicate();
+	}
+	void Communicate()
+	{
+		try
+		{
+			BufferedReader kRead = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter serverOut = new PrintWriter(client.getoutputStream(), true);
+			BufferedReader serverRead = new BufferedReader(new InputStreamReader
+				(client.getInputStream()));
+			while(true)
+			{
+				String c_cmd = serverRead.readLine();
+				System.out.println("Client :: " + c_cmd);
+				String s_cmd = kRead.readLine();
+				serverOut.println(s_cmd);
+			}
+		}catch (Exception e)
+		{
+			System.out.println("Connection lost...");
+		}
+	}
 	public static void main(String args[])
 	{
-	
+		try
+		{
+			int portNumber = Integer.parseInt(args[0]);
+			ServerSocket ss = new ServerSocket(portNumber);
+			ServerSocket ss1 = new ServerSocket(portNumber);
+			System.out.println("Server Loaded");
+			Socket s = ss.accept();
+			Socket s1 = ss1.accept();
+			System.out.println("Client connected...");
+			new Server(s1);
+			new Server(s);
+		}catch (Exception e)
+		{
+			System.out.println("Connection lost...");
+		}
 	}
 
 }
