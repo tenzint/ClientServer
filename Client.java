3c9ffe135672049fe5cf6e43812bf06c990da6aa
Tenzin Thabkhae <tenzin392@gmail.com>
2016-10-17 12:53:17 -0400

    Working copy of a simple server

--- Client.java
+++ Client.java
@@ -5,23 +5,24 @@ class Client
 {
 	public static void main(String args[])
 	{
-		String hostName = args[0];
-		int portNumber = Integer.parseInt(args[1]);
-		String fromServer="";
 		try {
-			Socket sock = new Socket(hostName, portNumber);
+			// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			Socket sock = new Socket("127.0.0.1", 1500);
 			
 			BufferedReader kRead = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Enter your  name: ");
+			String name = kRead.readLine();
 			PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			
 			while(true)
 			{
+				System.out.print(name + " - "); 
 				String c_cmd = kRead.readLine();
 				out.println(c_cmd);
 				
 				String s_cmd = in.readLine();
-				System.out.println("Server :: " +s_cmd);
+				System.out.println("Server - " +s_cmd);
 			}
 		} catch (Exception e)
 		{
--- Server.java
+++ Server.java
@@ -3,45 +3,30 @@ import java.net.*;
 
 class Server
 {
-	Socket client;
-	Server(Socket client)
-	{
-		this.client=client;
-		Communicate();
-	}
-	void Communicate()
-	{
-		try
-		{
-			BufferedReader kRead = new BufferedReader(new InputStreamReader(System.in));
-			PrintWriter serverOut = new PrintWriter(client.getoutputStream(), true);
-			BufferedReader serverRead = new BufferedReader(new InputStreamReader
-				(client.getInputStream()));
-			while(true)
-			{
-				String c_cmd = serverRead.readLine();
-				System.out.println("Client :: " + c_cmd);
-				String s_cmd = kRead.readLine();
-				serverOut.println(s_cmd);
-			}
-		}catch (Exception e)
-		{
-			System.out.println("Connection lost...");
-		}
-	}
+
 	public static void main(String args[])
 	{
 		try
 		{
-			int portNumber = Integer.parseInt(args[0]);
-			ServerSocket ss = new ServerSocket(portNumber);
-			ServerSocket ss1 = new ServerSocket(portNumber);
+			System.out.print("Enter your name: ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String name = br.readLine();
+			
+			ServerSocket ss = new ServerSocket(1500);
 			System.out.println("Server Loaded");
 			Socket s = ss.accept();
-			Socket s1 = ss1.accept();
-			System.out.println("Client connected...");
-			new Server(s1);
-			new Server(s);
+			System.out.println("Client connecting..." );
+			PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			while(true){
+				
+				String str  = in.readLine();
+				System.out.println("From client - " +str);
+				// Intermediate book: "Complete Reference"
+				System.out.print(name + " - ");
+				String  n = br.readLine();
+				out.println(n);
+			}
 		}catch (Exception e)
 		{
 			System.out.println("Connection lost...");
